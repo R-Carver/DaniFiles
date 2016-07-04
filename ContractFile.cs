8ee39610c3ce38565b63f36ba2e043b25eab9fe8
R-Carver <davidmakowski00@gmail.com>
2016-07-04 19:11:16 +0200

    Add files via upload

---
+++ ContractFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ContractFile
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        [Required]
+        [DataType(DataType.Url)]
+        public string FileUrl { get; set; }
+
+        public virtual Contract Contract { get; set; }
+    }
+}
